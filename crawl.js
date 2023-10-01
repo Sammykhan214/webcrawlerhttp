1409cb09f51cc498b94b2a0026a83932011516be
Sammykhan214 <110401447+Sammykhan214@users.noreply.github.com>
2023-10-02 01:32:20 +0530

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,4 +1,34 @@
+const {JSDOM} = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for(const linkElement of linkElements){
+        if(linkElement.href.slice(0,1)==='/'){
+
+            try{
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            }catch(err){
+                console.log(`error with relative URL: ${err.message}`)
+            }
+            
+        }else{
+            try{
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            }catch(err){
+                console.log(`error with relative URL: ${err.message}`)
+            }
+        }
+        
+    }
+    return urls;
+}
+
 function normalizeURL(urlString){
+    
     const url = new URL(urlString)
     const hostPath = `${url.hostname}${url.pathname}`;
     if(hostPath.length>0 && hostPath.slice(-1) === '/'){
@@ -11,5 +41,6 @@ function normalizeURL(urlString){
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
